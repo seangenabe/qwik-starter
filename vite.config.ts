de7d9609bff55dfd2b2f2acb8def8d46e2c03932
Sean Marvi Oliver Genabe <seanmarviolivergenabe@gmail.com>
2023-12-30 20:37:31 +0800

    add pandacss

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,20 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import { macroPlugin } from "@builder.io/vite-plugin-macro";
 export default defineConfig(() => {
-  return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-    dev: {
-      headers: {
-        "Cache-Control": "public, max-age=0",
-      },
-    },
-    preview: {
-      headers: {
-        "Cache-Control": "public, max-age=600",
-      },
-    },
-  };
+    return {
+        plugins: [
+            macroPlugin({ preset: "pandacss" }), qwikCity(), qwikVite(), tsconfigPaths()],
+        dev: {
+            headers: {
+                "Cache-Control": "public, max-age=0",
+            },
+        },
+        preview: {
+            headers: {
+                "Cache-Control": "public, max-age=600",
+            },
+        }
+    };
 });
